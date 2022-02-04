d3dbce133e5997caec7442455fd7f779731f3007
Chen <neil_chen_0918@live.cn>
2022-02-04 20:43:58 +0800

    add project Netcool.Core.Utility

---
+++ src/Netcool.Core.Utility/Helpers/Common.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Netcool.Core;
+
+public static class Common
+{
+    public static Type GetType<T>()
+    {
+        return GetType(typeof(T));
+    }
+
+    public static Type GetType(Type type)
+    {
+        return Nullable.GetUnderlyingType(type) ?? type;
+    }
+
+    public static T SafeValue<T>(this T? value) where T : struct
+    {
+        return value ?? default(T);
+    }
+
+    public static string SafeString(this object input)
+    {
+        return input?.ToString()?.Trim() ?? string.Empty;
+    }
+
+    public static bool IsLinux => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+    public static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+    public static bool IsOsx => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+    public static string System => IsWindows ? "Windows" : IsLinux ? "Linux" : IsOsx ? "OSX" : string.Empty;
+}
