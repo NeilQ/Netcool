e03dd423aea777ff2f152ecf75503a5e3882efd0
chen <neil_chen_0918@live.cn>
2024-09-05 19:57:07 +0800

    chore: upgrade angular 18 & ng-alain 18

--- src/fontend/Netcool.Admin/ClientApp/src/app/core/net/default.interceptor.ts
+++ src/fontend/Netcool.Admin/ClientApp/src/app/core/net/default.interceptor.ts
@@ -1,10 +1,4 @@
-import {
-  HttpErrorResponse,
-  HttpHandlerFn,
-  HttpInterceptorFn,
-  HttpRequest, HttpResponse,
-  HttpResponseBase
-} from '@angular/common/http';
+import { HttpErrorResponse, HttpHandlerFn, HttpInterceptorFn, HttpRequest, HttpResponse, HttpResponseBase } from '@angular/common/http';
 import { Injector, inject } from '@angular/core';
 import { IGNORE_BASE_URL } from '@delon/theme';
 import { environment } from '@env/environment';
